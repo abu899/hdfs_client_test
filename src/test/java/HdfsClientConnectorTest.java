69d45d70dbbdf52caba53945bd3e513f9398331d
Ahn ByungUk <byunguk.ahn@innowireless.com>
2022-02-24 19:25:30 +0900

    [HDFS] Namenode standalone test
      + standalone(namenode + datanode) test
      + change readUTF to byte read
        + String with readUTF, error return

--- src/test/java/HdfsClientConnectorTest.java
+++ src/test/java/HdfsClientConnectorTest.java
@@ -1,15 +1,14 @@
 import HdfsClient.HdfsClient;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
@@ -41,6 +40,13 @@ public class HdfsClientConnectorTest {
     void writeAndReadTest() {
         Configuration configure = new Configuration();
 
+//        System.setProperty("HADOOP_USER_NAME", "root");
+//        configure.set("HADOOP_USER_NAME", "root");
+//        configure.set("HADOOP_NAMENODE_USER", "root");
+//        configure.set("HADOOP_DATANODE_USER", "root");
+
+        configure.set("HADOOP_HOE", "/opt/hadoop");
+
         final String path = "/test_file/hello.txt";
         Path writePathInHDFS = new Path(path);
         FileSystem hdfs = null;
@@ -49,18 +55,34 @@ public class HdfsClientConnectorTest {
 
             //write
             final String testString = "test hello\n";
-//            FSDataOutputStream outStream = hdfs.create(writePath);
+//            FSDataOutputStream outStream = hdfs.create(writePathInHDFS);
 //            outStream.writeUTF(testString);
 //            outStream.close();
 
             //read
+//            int fileSize = 0;
+            FileStatus fileStatus = hdfs.getFileStatus(writePathInHDFS);
             FSDataInputStream inputStream = hdfs.open(writePathInHDFS);
-            String readString = inputStream.readUTF();
+//            String readString = inputStream.readUTF();
+            int data = 0;
+            StringBuilder readString = new StringBuilder();
+            while((data = inputStream.read()) != -1){
+                readString.append((char) data);
+            }
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate((int) fileStatus.getLen());
+            inputStream.readFully(0, byteBuffer);
+//            byte[] expected = new byte[locations.length];
+//            inputStream.readFully(0, expected);
+//            String readString = expected.toString();
             inputStream.close();
-            assertThat(readString).isEqualTo(testString);
+
+            String byteBufferStr = StandardCharsets.UTF_8.decode(byteBuffer).toString();
+            assertThat(readString.toString()).isEqualTo(testString);
             System.out.println("readString = " + readString);
         } catch (IOException e) {
             e.printStackTrace();
+            fail("fail write and read");
         }
 
         try {
