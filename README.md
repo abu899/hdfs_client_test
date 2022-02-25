01673c62f3891fa289921e7e58d43f1f4b16b071
Ahn ByungUk <byunguk.ahn@innowireless.com>
2022-02-25 17:14:22 +0900

    [HDFS] Use ByteBufferReader and add README.md contents
     + use ByteBufferReader when read linux file
     + add test code for writing and reading through java
     + README.md contents
       + error case and solution

--- README.md
+++ README.md
@@ -45,3 +45,84 @@ At hdfs_docker/datanode
    2. 2번에서 얻은 ip address 추가
 4. service ssh start (namenode, datanode)
 5. start-all.sh
+
+## Error 정리
+
+### Java ReadUTF()
+
+`HdfsClientConnectorTest.java` 의 `readSimpleTextTest`와 `readWriteWithJavaTest` method 참조
+
+- `readSimpleTextTest`의 read 대상이 되는 파일은 linux 상에서 직접 hdfs를 통해 put 해준 데이터이다.
+- 이 파일을 `readUTF()`를 통해 읽게 되면 `EOFException`이 발생하게 된다
+- Linux에는 String 타입이 존재하지 않기 때문에 `readUTF()`로 읽게되면 String 데이터가 존재하지 않기 때문에 Exception이 나온다.
+- 이런 경우 byte 형태로 한 글자씩 읽은 후 char로 변환하는 방법과 `ByteBufferReader`를 통해 읽을 수 있다
+- 반면 `readWriteWithJavaTest`에서는 자바로 String 형태로 데이터를 입력해주기 때문에 `readUTF()`로 데이터를 읽어도
+오류가 발생하지 않는다!!
+
+### Hadoop - Docker datanode access timeout or fail
+
+- Client에서 hadoop을 접근할 때, timeout이나 fail이 되는 경우가 존재하는데 에러를 보면 접근하는 ip가 docker 내부 ip인 경우가 존재한다
+```text
+WARN impl.BlockReaderFactory: I/O error constructing remote block reader.
+org.apache.hadoop.net.ConnectTimeoutException: 60000 millis timeout while waiting for channel to be ready for connect. 
+ch : java.nio.channels.SocketChannel[connection-pending remote=/172.17.0.2:9866]
+at org.apache.hadoop.net.NetUtils.connect(NetUtils.java:589)
+```
+- `connection-pending remote=/172.17.0.2:9866`와 같이 도커 내부에서 설정한 ip가 나오게되는데, 원래 같은 경우
+접속을 원하는 datanode로 접근해야 데이터를 읽어올 수 있게 된다
+- 이런 경우 `dfs.client.use.datanode.hostname` 를 `true`로 설정해줘서 client가 datanode를 hostname으로 접근하게 바꿔준다.
+- 이때 `/etc/hosts`에 datanode의 주소가 mapping되어 있지 않으면 `UnKnownHostException`이 발생할 수 있으니 참고해야 한다
+
+### UnKnownHostException
+
+- 앞서 설명한 timeout 및 fail 이외에 namenode 접근시에도 발생할 수 있는 오류로 로그상 접근하려는 주소가 hostname으로 되어있지만,
+정작 hostname의 주소가 mapping되어 있지 않은 경우 발생했다
+- `/etc/hosts`에 namenode의 hostname과 ip주소를 넣어주자!
+
+### NoRouteToHostException
+
+구글에서 찾아본 결과 여러가지 상황에서 발생했지만, 개발 중에는 두 가지를 시도하였고 해결되었다.
+
+1. firewall
+   - 방화벽에서 특정 port로 들어오는 패킷자체를 막아버리는 경우
+   - 이 경우 namenode나 datanode이 들어오는 port를 확인하고 port를 오픈해주는 방법이 존재한다
+   - 아니면 그냥 방화벽을 내려버리면 되지만.. 이 방법은 크게 추천하지 않고 테스트할 때 사용하는게 좋을 것 같다
+   - `iptables` 또는 `firewall-cmd`를 통해 해결해보자
+
+2. Docker 및 route 확인
+   - 동료 개발자가 이 문제로 `NoRouteToHostException` 문제가 생겼다
+   - Route는 루트가 아닌 라우터로, path 문제가 아니라는 점을 인지해야했다.
+   - 문제 발생 상황 당시, 개발자의 pc에 docker container가 올라가 있는 상태로서 docker의 ip로 라우팅 되고 있었다
+   - 즉, docker로 route된 패킷이 필터링되어 버리면서 문제가 생긴 것으로 유추된다.
+     - docker contatiner를 모두 내리고 `ifconfig docker0 down`을 실행하니 정상 동작되었다.
+   - 비슷한 라우팅 문제로 에러가 발생한 글이 있어 참조 남긴다
+     - [RoutingError Post](https://dd00oo.tistory.com/entry/java-NoRouteToHost-%ED%98%B8%EC%8A%A4%ED%8A%B8%EB%A1%9C-%EA%B0%88-%EB%A3%A8%ED%8A%B8%EA%B0%80-%EC%97%86%EC%9D%8C-%EC%97%90%EB%9F%AC)
+
+### BlockMissingError
+
+개발 중에 서버를 올리고 내리면서 가장 빈번하게 발생했던 error. 여러가지 원인이 존재하는 것 같지만 겪어본 내용들만 정리해보았다.
+
+1. File not found
+   - 단순하게 파일의 경로를 잘못 입력했을 때.
+   - block missing error는 아니지만 block의 위치를 아예 찾지 못하는 것이므로 같이 정리해보았다.
+   - 단순 로그 확인만으로도 진단 가능하고, 경로 재조정만 해주면 된다
+
+2. Namenode가 block의 위치를 정상적으로 가져오지만, 실제 데이터는 읽을 수 없을 때.
+   - Missing Block
+      - namenode와 datanode를 올리고 내리다 보면 발생했던 error.
+      - namenode에 기록되어있는 block의 위치와 실제 datanode의 위치가 다를 때 발생
+        - `hdfs dfsadmin -report`를 통해 확인할 수 있다
+      - namenode를 foramt하고 다시 파일을 넣어 block 위치를 맞춰줘야한다
+      - `hdfs namnode -format`
+
+  - datanode 접근 error
+    - Missing block이 없고 block의 위치와 datanode 내 기록된 block 정보도 올바른데 못 읽어오는 경우
+    - 정확히 어떤 문제인지 진단하기쉽지 않았던 error
+    - 시도했던 방법
+      - `UnKnownHostException`와 마찬가지로 namenode에 datanode의 주소를 mapping 해준다
+      - `service ssh start` ssh가 실행이 중지된 경우에도 발생
+      - docker로 hdfs를 구현할 시, 간혹 datanode 접근 시 conatiner id로 접근하려고 하는 경우,
+      datanode의 hostname을 datanode의 이름으로 변경 필요
+      - datanode의 ipc port가 제대로 열려있는지 확인
+      - hdfs-site.xml에서 configuration 변경
+        - `dfs.datanode.use.datanode.hostname`, `dfs.client.use.datanode.hostname`, 기타 등등..
--- src/test/java/HdfsClientConnectorTest.java
+++ src/test/java/HdfsClientConnectorTest.java
@@ -4,7 +4,9 @@ import org.apache.hadoop.fs.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
@@ -37,15 +39,8 @@ public class HdfsClientConnectorTest {
     }
 
     @Test
-    void writeAndReadTest() {
+    void readSimpleTextTest() {
         Configuration configure = new Configuration();
-
-//        System.setProperty("HADOOP_USER_NAME", "root");
-//        configure.set("HADOOP_USER_NAME", "root");
-//        configure.set("HADOOP_NAMENODE_USER", "root");
-//        configure.set("HADOOP_DATANODE_USER", "root");
-
-        configure.set("HADOOP_HOE", "/opt/hadoop");
 
         final String path = "/test_file/hello.txt";
         Path writePathInHDFS = new Path(path);
@@ -53,32 +48,16 @@ public class HdfsClientConnectorTest {
         try {
             hdfs = FileSystem.get(URI.create(url), configure);
 
-            //write
             final String testString = "test hello\n";
-//            FSDataOutputStream outStream = hdfs.create(writePathInHDFS);
-//            outStream.writeUTF(testString);
-//            outStream.close();
 
-            //read
-//            int fileSize = 0;
-            FileStatus fileStatus = hdfs.getFileStatus(writePathInHDFS);
             FSDataInputStream inputStream = hdfs.open(writePathInHDFS);
-//            String readString = inputStream.readUTF();
-            int data = 0;
-            StringBuilder readString = new StringBuilder();
-            while((data = inputStream.read()) != -1){
-                readString.append((char) data);
-            }
 
-            ByteBuffer byteBuffer = ByteBuffer.allocate((int) fileStatus.getLen());
-            inputStream.readFully(0, byteBuffer);
-//            byte[] expected = new byte[locations.length];
-//            inputStream.readFully(0, expected);
-//            String readString = expected.toString();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            String readString = br.readLine();
+
             inputStream.close();
 
-            String byteBufferStr = StandardCharsets.UTF_8.decode(byteBuffer).toString();
-            assertThat(readString.toString()).isEqualTo(testString);
+            assertThat(readString).isEqualTo(testString);
             System.out.println("readString = " + readString);
         } catch (IOException e) {
             e.printStackTrace();
@@ -91,5 +70,41 @@ public class HdfsClientConnectorTest {
             e.printStackTrace();
         }
 
+    }
+
+    @Test
+    void readWriteWithJavaTest(){
+        Configuration configure = new Configuration();
+
+        final String path = "/test_file/hello_2.txt";
+        Path writePathInHDFS = new Path(path);
+        FileSystem hdfs = null;
+        try {
+            hdfs = FileSystem.get(URI.create(url), configure);
+
+            //write
+            final String testString = "test hello java";
+            FSDataOutputStream outStream = hdfs.create(writePathInHDFS);
+            outStream.writeUTF(testString);
+            outStream.close();
+
+            //read
+            FSDataInputStream inputStream = hdfs.open(writePathInHDFS);
+            String readString = inputStream.readUTF();
+
+            inputStream.close();
+
+            assertThat(readString).isEqualTo(testString);
+            System.out.println("readString = " + readString);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("fail write and read");
+        }
+
+        try {
+            hdfs.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
